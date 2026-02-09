b5f05ff80bb778a058b4e0376c2d6e97db8909db
Martinezworldwide <34616499+Martinezworldwide@users.noreply.github.com>
2026-02-08 21:37:29 -0500

    Facebook feed: multi-player grid, 20 per page, pagination; add facebook-pages.js

---
+++ js/facebook-pages.js
@@ -0,0 +1,47 @@
+/**
+ * Soccer players' official Facebook Page usernames for the Facebook feed.
+ * Each entry: { fullName: string, pageUsername: string }.
+ * pageUsername = the part after facebook.com/ (e.g. leomessi -> https://www.facebook.com/leomessi)
+ * Add or edit entries to change the feed list.
+ */
+window.FACEBOOK_PAGES = [
+    { fullName: "Leo Messi", pageUsername: "leomessi" },
+    { fullName: "Cristiano Ronaldo", pageUsername: "Cristiano" },
+    { fullName: "Neymar Jr", pageUsername: "neymarjr" },
+    { fullName: "Kylian Mbappé", pageUsername: "KylianMbappe" },
+    { fullName: "Mohamed Salah", pageUsername: "MoSalah" },
+    { fullName: "Sergio Ramos", pageUsername: "SergioRamos" },
+    { fullName: "David Beckham", pageUsername: "DavidBeckham" },
+    { fullName: "Zlatan Ibrahimović", pageUsername: "IbraOficial" },
+    { fullName: "Paul Pogba", pageUsername: "PaulPogba" },
+    { fullName: "Andrés Iniesta", pageUsername: "andresiniesta" },
+    { fullName: "Toni Kroos", pageUsername: "tonikroos" },
+    { fullName: "Luka Modrić", pageUsername: "LukaModric" },
+    { fullName: "Eden Hazard", pageUsername: "hazardeden10" },
+    { fullName: "Antoine Griezmann", pageUsername: "AntoineGriezmann" },
+    { fullName: "Harry Kane", pageUsername: "HarryKane" },
+    { fullName: "Marcus Rashford", pageUsername: "MarcusRashford" },
+    { fullName: "Raheem Sterling", pageUsername: "sterling7" },
+    { fullName: "Virgil van Dijk", pageUsername: "virgilvandijk" },
+    { fullName: "Sadio Mané", pageUsername: "sadiomaneofficiel" },
+    { fullName: "Kevin De Bruyne", pageUsername: "KevinDeBruyne" },
+    { fullName: "Erling Haaland", pageUsername: "ErlingHaaland" },
+    { fullName: "Phil Foden", pageUsername: "PhilFoden" },
+    { fullName: "Bukayo Saka", pageUsername: "BukayoSaka" },
+    { fullName: "Jordi Alba", pageUsername: "JordiAlba" },
+    { fullName: "Gerard Piqué", pageUsername: "3gerardpique" },
+    { fullName: "Ronaldinho", pageUsername: "ronaldinho" },
+    { fullName: "Karim Benzema", pageUsername: "Benzema" },
+    { fullName: "Robert Lewandowski", pageUsername: "RobertLewandowski" },
+    { fullName: "Manuel Neuer", pageUsername: "ManuelNeuer" },
+    { fullName: "Thomas Müller", pageUsername: "ThomasMueller" },
+    { fullName: "Joshua Kimmich", pageUsername: "JoshuaKimmich" },
+    { fullName: "Leroy Sané", pageUsername: "leroysane" },
+    { fullName: "Serge Gnabry", pageUsername: "SergeGnabry" },
+    { fullName: "Kingsley Coman", pageUsername: "KingsleyComan" },
+    { fullName: "Alphonso Davies", pageUsername: "AlphonsoDavies" },
+    { fullName: "Pedri", pageUsername: "pedrigonzalez" },
+    { fullName: "Gavi", pageUsername: "gavi" },
+    { fullName: "Frenkie de Jong", pageUsername: "frenkiedejong" },
+    { fullName: "Vinicius Jr", pageUsername: "viniciusjr" }
+];
